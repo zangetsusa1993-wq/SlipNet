e68f9500cd58076f9e070bec673db8fd1f47a32e
anonvector <258689915+anonvector@users.noreply.github.com>
2026-03-06 13:48:35 +0100

    Bump to v2.2.1 (code 36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
 }
 
 val minSdkVersion = 24
-val appVersionName = "2.2.0"
-val appVersionCode = 35
+val appVersionName = "2.2.1"
+val appVersionCode = 36
 val cargoProfile = (findProperty("CARGO_PROFILE") as String?) ?: run {
     val isRelease = gradle.startParameter.taskNames.any { it.contains("Release", ignoreCase = true) }
     if (isRelease) "release" else "debug"
